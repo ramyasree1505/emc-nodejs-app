9f0c3c975d125d60e56d21b79f9aaaf924745de2
Venkiemc <Venkatesh.m@errormakesclever.com>
2025-05-18 16:46:06 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,8 +3,33 @@ const http = require('http');
 const port = 3000;
 
 const server = http.createServer((req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end("Hello from EMC Node.js App!\n");
+  res.writeHead(200, { 'Content-Type': 'text/html' });
+  res.end(`
+    <!DOCTYPE html>
+    <html>
+    <head>
+      <title>EMC Node.js App</title>
+      <style>
+        body {
+          display: flex;
+          justify-content: center;
+          align-items: center;
+          height: 100vh;
+          margin: 0;
+          font-family: Arial, sans-serif;
+          background-color: #f0f0f0;
+        }
+        h1 {
+          font-size: 48px;
+          color: #333;
+        }
+      </style>
+    </head>
+    <body>
+      <h1>congratulations!!! from EMC Node.js App!! All the Best!</h1>
+    </body>
+    </html>
+  `);
 });
 
 server.listen(port, () => {
