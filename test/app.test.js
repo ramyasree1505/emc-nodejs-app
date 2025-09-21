35050da446e2a6001cda87170b3718c666a1007d
Ramyasree_githubuser <52415341+ramyasree1505@users.noreply.github.com>
2025-09-21 21:48:52 +0530

    Update app.test.js

--- test/app.test.js
+++ test/app.test.js
@@ -15,7 +15,7 @@ after(() => {
 
 describe('GET /', () => {
   it('should return 200 OK and contain the expected message', (done) => {
-    supertest(server)
+    supertest()
       .get('/')
       .expect(200)
       .end((err, res) => {
